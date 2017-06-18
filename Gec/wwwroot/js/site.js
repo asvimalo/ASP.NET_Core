6cd2de3a7b10e428ea427096d33a50ba47f78a04
asvimalo <asvimalo@gmail.com>
2017-06-18 23:58:51 +0200

    logo

--- Gec/wwwroot/js/site.js
+++ Gec/wwwroot/js/site.js
@@ -1,23 +1,4 @@
-﻿// #region First code 
-
-//function startup() {
-
-//    var element = document.getElementById("username");
-//    element.innerHTML = "Maria Paula Vilhena Mascarenhas";
-
-//    var main = document.getElementById("main");
-
-//    main.onmouseenter = () => {
-//        main.style.background = "#888";
-//    }
-//    main.onmouseleave = () => {
-//        main.style.background = "";
-//    }
-//}
-
-//startup();
-
-// #endregion
+﻿
 
 $(document).ready(() => {
 
@@ -25,26 +6,6 @@ $(document).ready(() => {
 
 (function () {
 
-    //var element = $("#username");
-    //element.text("Maria Paula Vilhena Mascarenhas");
-
-    //var main = $("#main");
-    //console.log(main)
-
-    //main.on("mouseenter", function () {
-    //    console.log("onmouseenter")
-    //    main.css( "background-color","#888");
-    //});
-    //main.on("mouseleave", function () {
-    //    console.log("onmouseleave")
-    //    main.css("background-color","");
-    //});
-    //var menuItems = $("ul.menu li a");
-    //menuItems.on("click", function () {
-    //    var me = $(this)
-    //    alert(me.text());
-    //})
-    
     // Toggle side bar
     var sidebarAndWrapper = $("#sidebar,#wrapper");
     var icon = $("#sidebarToggle i.fa");
