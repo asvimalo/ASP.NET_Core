efdaf33dd53d8df3841133fb140b2f347e7308cd
asvimalo <asvimalo@gmail.com>
2017-06-22 15:05:26 +0200

    Some redirection changes

---
+++ Gec/Controllers/Web/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gec.ViewModels;
+using Gec.Services;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Gec.Controllers.Web
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+    }
+}
